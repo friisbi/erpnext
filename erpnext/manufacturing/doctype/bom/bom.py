5b1674018b9e58d60fe657b0494c4901dec7e01c
Mihir Kandoi <kandoimihir@gmail.com>
2025-11-19 12:11:56 +0530

    fix: redundant message on bom save
    (cherry picked from commit 074f07694fb04553c034ca84d0f7ec7797bbec7c)
    
    # Conflicts:
    #	erpnext/manufacturing/doctype/bom/bom.py

--- erpnext/manufacturing/doctype/bom/bom.py
+++ erpnext/manufacturing/doctype/bom/bom.py
@@ -465,7 +465,7 @@ class BOM(WebsiteGenerator):
 				)
 			)
 
-	def get_rm_rate(self, arg):
+	def get_rm_rate(self, arg, notify=True):
 		"""Get raw material rate as per selected method, if bom exists takes bom cost"""
 		rate = 0
 		if not self.rm_cost_as_per:
@@ -491,7 +491,7 @@ class BOM(WebsiteGenerator):
 								),
 								alert=True,
 							)
-						else:
+						elif notify:
 							frappe.msgprint(
 								_("{0} not found for item {1}").format(self.rm_cost_as_per, arg["item_code"]),
 								alert=True,
@@ -796,7 +796,13 @@ class BOM(WebsiteGenerator):
 						"stock_uom": d.stock_uom,
 						"conversion_factor": d.conversion_factor,
 						"sourced_by_supplier": d.sourced_by_supplier,
+<<<<<<< HEAD
 					}
+=======
+						"is_phantom_item": d.is_phantom_item,
+					},
+					notify=False,
+>>>>>>> 074f07694f (fix: redundant message on bom save)
 				)
 
 			d.base_rate = flt(d.rate) * flt(self.conversion_rate)
